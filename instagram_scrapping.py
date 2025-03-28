06bb9f59514a1780fb44c693c238ec229f5033cf
NasirMehmood19 <nasirmehmood2839@gmail.com>
2025-03-28 12:17:56 +0500

    Update instagram_scrapping.py

--- instagram_scrapping.py
+++ instagram_scrapping.py
@@ -8,6 +8,7 @@ import cloudinary.uploader
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from datetime import datetime
 import os
 
